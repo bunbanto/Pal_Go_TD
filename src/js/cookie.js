a79a86de08279ce0ce834af26828bd0b58fd1dd4
yurii_fishbakh <bunbanto@gmail.com>
2025-06-02 10:59:34 +0200

    cookie accept

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,8 +1,15 @@
 const popup = document.getElementById('cookiePopup');
 const buttons = document.querySelectorAll('.cookie-buttons .btn');
 
+if (localStorage.getItem('cookiesAccepted') === 'true') {
+  popup.style.display = 'none';
+}
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
+    if (button.classList.contains('accept')) {
+      localStorage.setItem('cookiesAccepted', 'true');
+    }
     popup.style.display = 'none';
   });
 });
