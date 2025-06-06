64b2d36a2e197e44485ea188da2c3ae601458f94
yurii_fishbakh <bunbanto@gmail.com>
2025-06-06 19:27:30 +0200

    fixed cookie

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,18 +1,28 @@
-const popup = document.getElementById('cookiePopup');
-const buttons = document.querySelectorAll('.cookie-buttons .btn');
+const popup = document.querySelector('[data-cookie-open]');
+export const popBtns = document.querySelectorAll('[data-btn-close]')
+const LS_KEY = 'pop-open';
 
-// Перевіряємо будь-яке рішення користувача
-if (localStorage.getItem('cookiesAccepted') !== null) {
-  popup.style.display = 'none';
+export function handleClosePopUp() {
+    if (!popup) return;
+    popup.dataset.cookieOpen = 'false';
+    setPopUp(false);
 }
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (button.classList.contains('accept')) {
-      localStorage.setItem('cookiesAccepted', 'true');
-    } else if (button.classList.contains('decline')) {
-      localStorage.setItem('cookiesAccepted', 'false');
-    }
-    popup.style.display = 'none';
-  });
+export function detectCookiePop() {
+    if (!popup) return;
+    const data = localStorage.getItem(LS_KEY);
+    const popUpState = data !== null ? JSON.parse(data) : true;
+    popup.dataset.cookieOpen = String(popUpState);
+}
+
+// Додаємо обробники для кнопок
+popBtns.forEach(btn => {
+  btn.addEventListener('click', handleClosePopUp);
 });
+
+// Викликаємо при завантаженні
+detectCookiePop();
+
+function setPopUp(state) {
+    localStorage.setItem(LS_KEY, JSON.stringify(state));
+};
