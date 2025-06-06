2efedd839857a00c380614679d4c1c2b94a4fe6f
yurii_fishbakh <bunbanto@gmail.com>
2025-06-07 00:35:58 +0200

    done

--- src/js/features.js
+++ src/js/features.js
@@ -1,18 +1,52 @@
+// import Swiper from 'swiper';
+// import { Navigation, Keyboard } from 'swiper/modules';
+// import 'swiper/css';
+
+// Swiper.use([Navigation, Keyboard]);
+
+// const swiper = new Swiper('#features-swiper', {
+//   loop: true,
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 20,
+//     },
+
+//     1200: {
+//       slidesPerView: 3,
+//       spaceBetween: 104,
+//     },
+//   },
+// });
 import Swiper from 'swiper';
-import { Navigation, Keyboard } from 'swiper/modules';
+import { Pagination, Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/pagination';
 
-Swiper.use([Navigation, Keyboard]);
+Swiper.use([Pagination, Navigation, Keyboard]);
 
-const swiper = new Swiper('.features-swiper', {
+const swiper = new Swiper('#features-swiper', {
   loop: true,
   keyboard: {
     enabled: true,
     onlyInViewport: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '[data-next="next"]',
+    prevEl: '[data-prev="prev"]',
+
+  },
+ pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
   breakpoints: {
     320: {
@@ -22,7 +56,7 @@ const swiper = new Swiper('.features-swiper', {
 
     1200: {
       slidesPerView: 3,
-      spaceBetween: 104,
+      spaceBetween: 40,
     },
   },
 });
